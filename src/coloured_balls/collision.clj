44c283c87d503213279070923872e16e2b379b64
Simon Katz <git@nomistech.com>
2011-06-02 17:41:48 +0100

    Fixed the readme file.

--- src/coloured_balls/collision.clj
+++ src/coloured_balls/collision.clj
@@ -1,10 +1,9 @@
 (ns coloured-balls.collision
-  (:use [clojure.contrib.math]))
+	(:use [clojure.contrib.math]))
 
 (defn distance [a b]
-  (let [x1 (:x a)
-        y1 (:y a)
-        x2 (:x b)
-        y2 (:y b)]
-    (sqrt (+ (expt (abs (- x1 x2)) 2)
-             (expt (abs (- y1 y2)) 2)))))
+	(let [ x1 (:x a)
+		y1 (:y a)
+		x2 (:x b)
+		y2 (:y b)]
+	(sqrt(+ (expt (abs (- x1 x2)) 2) (expt (abs (- y1 y2)) 2)))))
--- src/coloured_balls/motion.clj
+++ src/coloured_balls/motion.clj
@@ -1,16 +1,15 @@
 (ns coloured-balls.motion
-  (:use [clojure.contrib.generic.math-functions :only (round, cos, sin)]))
-
+	(:use [clojure.contrib.generic.math-functions :only (round, cos, sin)]))
+	
 (defn degrees-to-radians [degrees]
-  (* (/ Math/PI 180) degrees))
+	(* (/ Math/PI 180) degrees))
 
 (defn move [item]
-  (let [heading (degrees-to-radians (:heading item))
-        velocity (:velocity item)
-        old-x (:x item)
-        old-y (:y item)
-        new-x (round (* velocity (cos heading)))
-        new-y (round (* velocity (sin heading)))]
-    (conj {:x (+ old-x new-x)
-           :y (+ old-y new-y)}
-          (dissoc item :x :y))))
+	(let [heading (degrees-to-radians (:heading item))
+		velocity (:velocity item)
+		old-x (:x item)
+		old-y (:y item)
+		new-x (round (* velocity (cos heading)))
+		new-y (round (* velocity (sin heading)))]
+		(conj {:x (+ old-x new-x) :y (+ old-y new-y)} (dissoc item :x :y))))
+		
